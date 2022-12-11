7dafc9d612eb0d52b6bc403135f33fe8757d5972
Rajeshds20 <dsrajesh71@gmail.com>
2022-12-11 15:20:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 console.log("Hello World!");
 
-const basepath = "/songs/"
+const basepath = "/songs/";
 
 const audio = document.getElementById("audio");
 const thumbnail = document.getElementById("thumbnail");
 const sname = document.getElementById("songname");
-
 
 const shuffle = document.getElementById("options1");
 const previous = document.getElementById("options2");
@@ -14,7 +13,6 @@ const pause = document.getElementById("options4");
 const next = document.getElementById("options5");
 const listsongs = document.getElementById("options6");
 
-
 const maxDuration = document.getElementById("max-duration");
 const currentTimeRef = document.getElementById("current-time");
 const progressBar = document.getElementById("progress-bar");
@@ -22,68 +20,68 @@ const currentProgress = document.getElementById("current-progress");
 
 // document.getElementById("player").style.display = "none";
 
-
-
-
-
 var currsong = audio.src;
 // console.log(currsong);
-currsong = currsong.split('/');
+currsong = currsong.split("/");
 currsong = currsong[currsong.length - 1];
 
-const songlist = ['Inthandam.mp3', 'Samajavaragamana.mp3', 'Khairiyat.mp3', 'Kesariya.mp3', 'Heat_Waves.mp3', 'Night_Changes.mp3', 'Inthandam.mp3'];
+const songlist = [
+    "Inthandam.mp3",
+    "Samajavaragamana.mp3",
+    "Khairiyat.mp3",
+    "Heat_Waves.mp3",
+    "Night_Changes.mp3",
+    "Ranjithame.mp3",
+    "Kesariya.mp3"
+];
 
 const songdetails = {
-    'Inthandam.mp3': {
-        name: 'Inthandam',
-        movie: 'Sita Ramam',
-        image: 'https://i.ytimg.com/vi/dOKQeqGNJwY/maxresdefault.jpg'
-
+    "Inthandam.mp3": {
+        name: "Inthandam",
+        movie: "Sita Ramam",
+        image: "https://i.ytimg.com/vi/dOKQeqGNJwY/maxresdefault.jpg"
     },
 
-    'Samajavaragamana.mp3': {
-        name: 'Samajavaragamana',
-        movie: 'Ala Vaikuntapuramu lo',
-        image: 'https://www.lyricbazar.com/wp-content/uploads/2020/08/samajawaragamana-song.jpg'
+    "Samajavaragamana.mp3": {
+        name: "Samajavaragamana",
+        movie: "Ala Vaikuntapuramu lo",
+        image:
+            "https://www.lyricbazar.com/wp-content/uploads/2020/08/samajawaragamana-song.jpg"
     },
 
-    'Khairiyat.mp3': {
-        name: 'Khairiyat',
-        movie: 'Chhichhore',
-        image: 'https://www.lyricszaade.com/wp-content/uploads/2020/07/khairiyat-chhichhore.jpg'
-    },
-    
-    'Heat_Waves.mp3': {
-        name: 'Heat Waves',
-        movie: 'Glass Animals',
-        image: 'https://i.ytimg.com/vi/48hULfubFBc/maxresdefault.jpg'
-    },
-    
-    'Kesariya.mp3': {
-        name: 'Kesariya',
-        movie: 'Brahmastra',
-        image: 'https://s.enavabharat.com/wp-content/uploads/2022/07/Kesariya-Brahmastra.jpg'
-    },
-    
-    'Aashiqui_aa_gayi.mp3': {
-        name: 'Aashiqui Aa Gayi',
-        movie: 'Radhe Shyam',
-        image: 'https://bollywoodproduct.in/wp-content/uploads/2021/11/maxresdefault-129-1068x601.jpg'
+    "Ranjithame.mp3": {
+        name: "Ranjithame",
+        movie: "Varisu",
+        image:
+            "https://secureservercdn.net/160.153.138.217/ljp.b8e.myftpupload.com/wp-content/uploads/2022/11/Ranjithame-song-Varisu.jpg?time=1667655733"
     },
 
-    'Night_Changes.mp3': {
-        name: 'Night Changes',
-        movie: 'One Direction',
-        image: 'https://i.ytimg.com/vi/7KPyunRIjr0/maxresdefault.jpg'
+    "Khairiyat.mp3": {
+        name: "Khairiyat",
+        movie: "Chhichhore",
+        image:
+            "https://www.lyricszaade.com/wp-content/uploads/2020/07/khairiyat-chhichhore.jpg"
     },
-}
 
+    "Kesariya.mp3": {
+        name: "Kesariya",
+        movie: "Brahmastra",
+        image:
+            "https://s.enavabharat.com/wp-content/uploads/2022/07/Kesariya-Brahmastra.jpg"
+    },
 
+    "Heat_Waves.mp3": {
+        name: "Heat Waves",
+        movie: "Glass Animals",
+        image: "https://i.ytimg.com/vi/48hULfubFBc/maxresdefault.jpg"
+    },
 
-
-
-
-
+    "Night_Changes.mp3": {
+        name: "Night Changes",
+        movie: "One Direction",
+        image: "https://i.ytimg.com/vi/7KPyunRIjr0/maxresdefault.jpg"
+    }
+};
 
 play.addEventListener("click", () => {
     audio.play();
@@ -102,7 +100,7 @@ pause.addEventListener("click", () => {
 next.addEventListener("click", () => {
     var index = songlist.indexOf(currsong);
     index++;
-    audio.src = basepath + songlist[index = index % songlist.length];
+    audio.src = basepath + songlist[(index = index % songlist.length)];
     console.log(index, songlist.length);
     currsong = songlist[index];
     thumbnail.src = songdetails[currsong].image;
@@ -120,10 +118,10 @@ next.addEventListener("click", () => {
 previous.addEventListener("click", () => {
     var index = songlist.indexOf(currsong);
     index--;
-    if (index == -1) {
+    if (index === -1) {
         index = songlist.length - 1;
     }
-    audio.src = basepath + songlist[index = index % songlist.length];
+    audio.src = basepath + songlist[(index = index % songlist.length)];
     audio.play();
     play.style.display = "none";
     pause.style.display = "inline";
@@ -149,14 +147,13 @@ listsongs.addEventListener("click", () => {
     if (document.getElementById("songslist").style.display == "inline") {
         document.getElementById("songslist").style.display = "none";
         document.getElementById("player").style.justifyContent = "center";
-    }
-    else {
+    } else {
         document.getElementById("player").style.justifyContent = "space-evenly";
         document.getElementById("songslist").style.display = "inline";
-        var listOfSongs = 'PlayList : <br/><br/>';
+        var listOfSongs = "PlayList : <br/><br/>";
         var i1 = songlist.indexOf(currsong);
         for (var i = i1; i <= songlist.length - 2; i++) {
-            listOfSongs += songdetails[songlist[i]].name + '<br/>';
+            listOfSongs += songdetails[songlist[i]].name + "<br/>";
         }
         document.getElementById("songslist").innerHTML = listOfSongs;
         // document.getElementById("songslist").innerText = 'sample';
@@ -167,29 +164,16 @@ audio.onended = () => {
     next.click();
 };
 
-
-
-
-
-
-
-
-
-
-
-
 let events = {
     mouse: {
-        click: "click",
+        click: "click"
     },
     touch: {
-        click: "touchstart",
-    },
+        click: "touchstart"
+    }
 };
 
 let deviceType = "";
-
-
 
 const isTouchDevice = () => {
     try {
@@ -236,8 +220,3 @@ progressBar.addEventListener(events[deviceType].click, (event) => {
 audio.addEventListener("timeupdate", () => {
     currentTimeRef.innerText = timeFormatter(audio.currentTime);
 });
-
-
-
-
-
